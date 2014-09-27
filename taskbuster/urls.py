8d71a1c7a4023031e144a958881c9022f606766b
Severo <severo.fabricio@gmail.com>
2014-09-27 19:58:56 +0000

    Base, Humans, Robot, index and favicon

--- taskbuster/urls.py
+++ taskbuster/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from .views import home
+from .views import home, home_files
 
 urlpatterns = patterns(
     '',
+    url(r'^(?P<filename>(robots.txt)|(humans.txt))$',
+        home_files, name='home-files'),
     url(r'^$', home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
